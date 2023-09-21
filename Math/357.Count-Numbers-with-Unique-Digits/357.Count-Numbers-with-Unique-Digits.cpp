99993cb6e5d16edb8f763d16efbfe981d5d84d0b
Arvin <Arvin@Arvins-MacBook-Air.local>
2023-09-21 16:46:49 +0800

    Create 357.Count-Numbers-with-Unique-Digits.cpp

---
+++ Math/357.Count-Numbers-with-Unique-Digits/357.Count-Numbers-with-Unique-Digits.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        int ret=1;        
+        for (int len=1; len<=n; len++)
+            ret+=A(10, len)-A(9, len-1);
+        return ret;
+    }
+    int A(int m, int n)
+    {
+        int ret=1;
+        for (int i=0; i<n; i++)
+            ret*=m-i;
+        return ret;
+    }
+};
