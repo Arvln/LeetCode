4b4c66697a6599c656a891edd242355541d5d2cb
Arvin <Arvin@Arvins-MacBook-Air.local>
2023-09-24 23:01:47 +0800

    Create 2461.Maximum-Sum-of-Distinct-Subarrays-With-Length-K.cpp

---
+++ Two_Pointers/Sliding_Window/2461.Maximum-Sum-of-Distinct-Subarrays-With-Length-K/2461.Maximum-Sum-of-Distinct-Subarrays-With-Length-K.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    long long maximumSubarraySum(vector<int>& nums, int k) {
+        int left=0, right=0, count=0;
+        long long ret=0, sum=0;
+        vector<int> records(100001, 0);
+        while (right<nums.size())
+        {
+            if (records[nums[right]]==0)
+            {
+                count+=1;
+                sum+=nums[right];
+            }
+            records[nums[right]]+=1;
+            right++;
+
+            while (left<right&&right-left==k)
+            {
+                if (count==k)
+                    ret=max(ret, sum);
+                if (records[nums[left]]==1)
+                {
+                    count-=1;
+                    sum-=nums[left];
+                }
+                records[nums[left]]-=1;
+                left++;
+            }
+        }
+        return ret;
+    }
+};
