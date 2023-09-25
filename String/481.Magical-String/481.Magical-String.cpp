b0ec7fc315e44d1ee5151811e0b4d766a4751900
Arvin <Arvin@Arvins-MacBook-Air.local>
2023-09-25 14:04:34 +0800

    Update 481.Magical-String.cpp

--- String/481.Magical-String/481.Magical-String.cpp
+++ String/481.Magical-String/481.Magical-String.cpp
@@ -4,13 +4,13 @@ public:
         string s="122";
         for (int i=2; s.size()<n; i++)
         {
+            char last=('3'-s.back())+'0';
             if (s[i]=='1')
-                s.push_back('0'+('3'-s.back()));
+                s.push_back(last);
             else
             {
-                char last=s.back();
-                s.push_back('0'+('3'-last));
-                s.push_back('0'+('3'-last));
+                s.push_back(last);
+                s.push_back(last);
             }
         }
         int ret=0;
