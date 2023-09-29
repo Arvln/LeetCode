f28fd01241f322722f65f982c1de1db32855dbb4
Arvin <Arvin@Arvins-MacBook-Air.local>
2023-09-29 15:03:24 +0800

    Create 313.Super-Ugly-Number.cpp

---
+++ Two_Pointers/313.Super-Ugly-Number/313.Super-Ugly-Number.cpp
@@ -0,0 +1,26 @@
+typedef pair<long, int> PLI;
+class Solution {
+public:
+    int nthSuperUglyNumber(int n, vector<int>& primes) {
+        int k=primes.size();
+        vector<int> points(k, 0);
+        vector<long> ret{1};
+        priority_queue<PLI, vector<PLI>, greater<>> pq;
+        for (int i=0; i<k; i++)
+            pq.push({ret[points[i]]*primes[i], i});
+        for (int p=0; p<n-1; p++)
+        {
+            long cur=pq.top().first;
+            ret.push_back(cur);
+
+            while (!pq.empty()&&cur==pq.top().first)
+            {
+                int i=pq.top().second;
+                pq.pop();
+                points[i]+=1;
+                pq.push({ret[points[i]]*primes[i], i});
+            }
+        }
+        return ret.back();
+    }
+};
