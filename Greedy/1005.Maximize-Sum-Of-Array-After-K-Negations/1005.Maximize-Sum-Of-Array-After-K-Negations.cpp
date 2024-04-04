873f2d7033d7273f99e34924d1b236a53674b5f6
Arvin <Arvin@apples-MacBook-Air.local>
2024-04-04 13:16:39 +0800

    Create 1005.Maximize-Sum-Of-Array-After-K-Negations.cpp

---
+++ Greedy/1005.Maximize-Sum-Of-Array-After-K-Negations/1005.Maximize-Sum-Of-Array-After-K-Negations.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int largestSumAfterKNegations(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int sum=0, count=0, minVal=100;
+        for (int num : nums)
+        {
+            if (num<0&&k>0)
+            {
+                k--;
+                num*=-1;
+            }
+            minVal=min(minVal, num);
+            sum+=num;
+        }
+
+        if (k%2==0) return sum;
+        else return sum-2*minVal;
+    }
+};
