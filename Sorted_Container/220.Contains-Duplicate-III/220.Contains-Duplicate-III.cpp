e8111222bc464f9d211672243e28f523e9b07b0e
Arvin <Arvin@apples-MacBook-Air.local>
2024-04-23 09:48:29 +0800

    Update 220.Contains-Duplicate-III.cpp

--- Sorted_Container/220.Contains-Duplicate-III/220.Contains-Duplicate-III.cpp
+++ Sorted_Container/220.Contains-Duplicate-III/220.Contains-Duplicate-III.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff, int valueDiff) {
-        set<int>Set;
-        Set.insert(nums[0]);
-        for (int i=1; i<nums.size(); i++)
+        multiset<int>Set;
+        for (int i=0; i<nums.size(); i++)
         {
-            if (i>indexDiff) Set.erase(nums[i-indexDiff-1]);
+            if (i>indexDiff) Set.erase(Set.lower_bound(nums[i-indexDiff-1]));
 
-            auto iter=Set.lower_bound(nums[i]-valueDiff);
-            if (iter!=Set.end()&&abs(*iter-nums[i])<=valueDiff) return true;
+            auto it=Set.lower_bound(nums[i]-valueDiff);
+            if (it!=Set.end()&&abs(*it-nums[i])<=valueDiff) return true;
 
             Set.insert(nums[i]);
         }
