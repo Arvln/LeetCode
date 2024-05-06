13bb5f3aa9382c85e5c2b6a718a0e8572d0bcbe6
Arvin <Arvin@apples-MacBook-Air.local>
2024-05-06 16:44:49 +0800

    Update 417.Pacific-Atlantic-Water-Flow.cpp

--- DFS/417.Pacific-Atlantic-Water-Flow/417.Pacific-Atlantic-Water-Flow.cpp
+++ DFS/417.Pacific-Atlantic-Water-Flow/417.Pacific-Atlantic-Water-Flow.cpp
@@ -1,39 +1,37 @@
 class Solution {
+    int dirs[4][2]{{1,0},{0,1},{-1,0},{0,-1}};
     int m, n;
+    vector<vector<int>>heights;
 public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         m=heights.size(), n=heights[0].size();
+        this->heights=heights;
         vector<vector<int>>pac(m, vector<int>(n, 0));
-        vector<vector<int>>alt(m, vector<int>(n, 0));
-
+        vector<vector<int>>atl(m, vector<int>(n ,0));
         for (int i=0; i<m; i++)
         {
-            dfs(i, 0, INT_MIN, heights, pac);
-            dfs(i, n-1, INT_MIN, heights, alt);
+            dfs(pac, i, 0, INT_MIN);
+            dfs(atl, i, n-1, INT_MIN);
         }
         for (int j=0; j<n; j++)
         {
-            dfs(0, j, INT_MIN, heights, pac);
-            dfs(m-1, j, INT_MIN, heights, alt);
+            dfs(pac, 0, j, INT_MIN);
+            dfs(atl, m-1, j, INT_MIN);
         }
-
-        vector<vector<int>>ret;
+        vector<vector<int>>rets;
         for (int i=0; i<m; i++)
             for (int j=0; j<n; j++)
-                if (pac[i][j]&&alt[i][j])
-                    ret.push_back({i, j});
-
-        return ret;
+                if (pac[i][j]==1&&atl[i][j]==1) rets.push_back({i, j});
+        return rets;
     }
-    void dfs(int i, int j, int height, vector<vector<int>>& heights, vector<vector<int>>& sea)
+    void dfs(vector<vector<int>> &visited, int i, int j, int preHeight)
     {
         if (i<0||j<0||i>=m||j>=n) return;
-        if (sea[i][j]||height>heights[i][j]) return;
+        else if (visited[i][j]==1) return;
+        else if (preHeight>heights[i][j]) return;
+        visited[i][j]=1;
 
-        sea[i][j]=1;
-        dfs(i-1, j, heights[i][j], heights, sea);
-        dfs(i+1, j, heights[i][j], heights, sea);
-        dfs(i, j-1, heights[i][j], heights, sea);
-        dfs(i, j+1, heights[i][j], heights, sea);
+        for (auto dir : dirs)
+            dfs(visited, i+dir[0], j+dir[1], heights[i][j]);
     }
 };
