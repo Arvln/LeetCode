2a81f783d3007a0f0e00e50c1e398977a8feade8
Arvin <Arvin@apples-MacBook-Air.local>
2024-05-16 11:14:58 +0800

    Create 141.Linked-List-Cycle.cpp

---
+++ Two_Pointers/141.Linked-List-Cycle/141.Linked-List-Cycle.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *f=head, *s=head;
+        while (f!=NULL&&f->next!=NULL)
+        {
+            f=f->next->next;
+            s=s->next;
+
+            if (f==s) return true;
+        }
+        return false;
+    }
+};
