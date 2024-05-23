448cf43ea2e707445fd4316998c38042d5049edd
Arvin <Arvin@apples-MacBook-Air.local>
2024-05-23 12:28:17 +0800

    Create 392.Is-Subsequence.cpp

---
+++ Dynamic_Programming/Double_Sequence/392.Is-Subsequence/392.Is-Subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int m=s.size(), n=t.size();
+        s="#"+s;
+        t="#"+t;
+        vector<vector<bool>>dp(m+1, vector<bool>(n+1, false));
+        for (int j=0; j<=n; j++)
+            dp[0][j]=true;
+
+        for (int i=1; i<=m; i++)
+            for (int j=1; j<=n; j++)
+                if (s[i]==t[j]) dp[i][j]=dp[i-1][j-1];
+                else dp[i][j]=dp[i][j-1];
+        return dp[m][n];
+    }
+};
