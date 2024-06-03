06a4bbd9bed18614668d7342dc46724d8b61255f
Arvin <Arvin@apples-MacBook-Air.local>
2024-06-03 11:31:27 +0800

    Update 79.Word-Search.cpp

--- DFS/79.Word-Search/79.Word-Search.cpp
+++ DFS/79.Word-Search/79.Word-Search.cpp
@@ -1,30 +1,28 @@
 class Solution {
+    int dirs[4][2]{{1,0},{0,1},{-1,0},{0,-1}};
     int m, n;
-    vector<pair<int, int>>dirs{{1,0}, {0,1}, {-1,0}, {0,-1}};
+    vector<vector<int>>visited;
+    string word;
 public:
     bool exist(vector<vector<char>>& board, string word) {
         m=board.size(), n=board[0].size();
+        visited=vector<vector<int>>(m, vector<int>(n, 0));
+        this->word=word;
         for (int i=0; i<m; i++)
             for (int j=0; j<n; j++)
-            {
-                if (board[i][j]!=word[0]) continue;
-                vector<vector<int>>visited(m, vector<int>(n, 0));
-                if(dfs(board, word, i, j, 0, visited))
-                    return true;
-            }
+                if (dfs(board, i, j, 0)) return true;
         return false;
     }
-    bool dfs(vector<vector<char>>& board, string& word, int i, int j, int k, vector<vector<int>>& visited)
+    bool dfs(vector<vector<char>> &board, int i, int j, int cur)
     {
-        if (k==word.size()) return true;
-        if (i<0||j<0||i>=m||j>=n) return false;
-        if (visited[i][j]) return false;
-        if (board[i][j]!=word[k]) return false;
-
+        if (cur==word.size()) return true;
+        else if (i<0||j<0||i>=m||j>=n) return false;
+        else if (board[i][j]!=word[cur]) return false;
+        else if (visited[i][j]==1) return false;
+        
         visited[i][j]=1;
-        for (auto [x, y] : dirs)
-            if (dfs(board, word, i+x, j+y, k+1, visited))
-                return true;
+        for (auto dir : dirs)
+            if (dfs(board, i+dir[0], j+dir[1], cur+1)) return true;
         visited[i][j]=0;
         return false;
     }
