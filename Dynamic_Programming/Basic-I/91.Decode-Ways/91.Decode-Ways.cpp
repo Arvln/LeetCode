55cd0118a878909638e5c94e5e71ebc9054f336e
Arvin <Arvin@apples-MacBook-Air.local>
2024-06-14 15:03:24 +0800

    Create 91.Decode-Ways.cpp

---
+++ Dynamic_Programming/Basic-I/91.Decode-Ways/91.Decode-Ways.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n=s.size();
+        s="#"+s;
+        vector<int>dp(n+1, 0);
+        dp[0]=1;
+
+        for (int i=1; i<=n; i++)
+        {
+            if (s[i]>'0') dp[i]+=dp[i-1];
+            if (i>=2&&s[i-1]=='1'||(s[i-1]=='2'&&s[i]<='6')) dp[i]+=dp[i-2];
+        }
+        return dp[n];
+    }
+};
